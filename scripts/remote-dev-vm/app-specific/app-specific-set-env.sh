274a49939cde5b815e41a836925e8075e03d3b81
Joe Rice <joericearchitect@gmail.com>
2017-04-12 19:12:46 -0400

    pushing latest changes before switching dev machines

--- scripts/remote-dev-vm/app-specific/app-specific-set-env.sh
+++ scripts/remote-dev-vm/app-specific/app-specific-set-env.sh
@@ -1,5 +1,5 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
-source $DIR/app-specific-set-env.sh
+source $DIR/setenv.sh
 
 $SCRIPTS_DIR/dev-commit-git-changes.sh site-joerice-architect $SITE_JARCH_GIT_PROJECT_DIR $1
