25879c488cc4db2ae63466403b659cf096b9066e
Joe Rice <joseph.rice@hitachivantara.com>
2017-10-20 21:20:06 -0400

    fixed a lot of issues with getting vagrant up to to start flawleslly

--- vagrant/provision/setup.sh
+++ vagrant/provision/setup.sh
@@ -1,47 +1,18 @@
 #!/bin/sh
 
-DEV_ENV_VM_GIT_PROJECTS_DIR = $1
-DEV_ENV_VM_APPS_DIR = $2
-DEV_ENV_VM_MAVEN_CONF_DIR = $3
-DEV_ENV_VM_MAVEN_REPO_DIR = $4
-DEV_ENV_VM_AWS_DIR = $5
-
 #
 # JRA Development Environment
 #  VM Provision Script - This script will "provision" the dev environment VM by installing all prerequisits.
 #  Note:  there may be other provisioners that run before or after this script.  Check the Vagrantfile.
 echo "."
-echo "*********************************************************************************
-echo "   Provisioning wdpr dev environment virtual machine..."
-echo "     GIT_PROJECTS_DIR = $GIT_PROJECTS_DIR "
-echo "     APPS_DIR = $APPS_DIR "
-echo "     MAVEN_CONF_DIR = $MAVEN_CONF_DIR "
-echo "     MAVEN_REPO_DIR = $MAVEN_REPO_DIR "
-echo "     AWS_DIR = $AWS_DIR "
-echo "   ."
-echo "     DEV_ENV_VM_GIT_PROJECTS_DIR = $DEV_ENV_VM_GIT_PROJECTS_DIR "
-echo "     DEV_ENV_VM_APPS_DIR = $DEV_ENV_VM_APPS_DIR "
-echo "     DEV_ENV_VM_MAVEN_CONF_DIR = $DEV_ENV_VM_MAVEN_CONF_DIR "
-echo "     DEV_ENV_VM_MAVEN_REPO_DIR = $DEV_ENV_VM_MAVEN_REPO_DIR "
-echo "     DEV_ENV_VM_AWS_DIR = $DEV_ENV_VM_AWS_DIR "
-echo "
-echo "*********************************************************************************
+echo "*********************************************************************************"
+echo "   Provisioning jra dev environment virtual machine..."
+echo "*********************************************************************************"
 
 echo "."
-echo "
-echo "   Setting up folder permissions..."
-echo "*********************************************************************************
-sudo chown -R vagrant:vagrant
-sudo chown -R vagrant:vagrant $DEV_ENV_VM_GIT_PROJECTS_DIR
-sudo chown -R vagrant:vagrant $DEV_ENV_VM_APPS_DIR
-sudo chown -R vagrant:vagrant $DEV_ENV_VM_MAVEN_CONF_DIR
-sudo chown -R vagrant:vagrant $DEV_ENV_VM_MAVEN_REPO_DIR
-sudo chown -R vagrant:vagrant $DEV_ENV_VM_AWS_DIR
-
-echo "."
-echo "*********************************************************************************
+echo "*********************************************************************************"
 echo "   updating apt-get and installing common linux tools"
-echo "*********************************************************************************
+echo "*********************************************************************************"
 sudo apt-get update -y 
 sudo apt-get install -y zip 
 sudo apt-get install -y unzip 
@@ -49,53 +20,53 @@ sudo apt-get install -y dos2unix
 
 
 echo "."
-echo "*********************************************************************************
+echo "*********************************************************************************"
 echo "   Installing Pip"
-echo "*********************************************************************************
+echo "*********************************************************************************"
 sudo wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 
 echo "."
-echo "*********************************************************************************
+echo "*********************************************************************************"
 echo "   Installing Git"
-echo "*********************************************************************************
+echo "*********************************************************************************"
 sudo apt-get install git -y 
 
 echo "."
-echo "*********************************************************************************
+echo "*********************************************************************************"
 echo "   Installing Maven"
-echo "*********************************************************************************
+echo "*********************************************************************************"
 sudo apt-get install maven -y 
 # for Maven, setting a new symbolic link so that maven points to the conf dir that is synced with host machine.
 sudo rm /usr/share/maven/conf
 sudo ln -s /home/vagrant/workspace/maven/conf /usr/share/maven/conf
 
 echo "."
-echo "*********************************************************************************
+echo "*********************************************************************************"
 echo "   Settng symbolic link for git"
-echo "*********************************************************************************
+echo "*********************************************************************************"
 sudo ln -s /home/vagrant/workspace/git ~/git
 
 echo "."
-echo "*********************************************************************************
+echo "*********************************************************************************"
 echo "   Installing Docker Machine"
-echo "*********************************************************************************
+echo "*********************************************************************************"
 sudo curl -L https://github.com/docker/machine/releases/download/v0.8.2/docker-machine-`uname -s`-`uname -m` >~/docker-machine
 sudo mv ~/docker-machine /usr/local/bin/docker-machine
 sudo chmod +x /usr/local/bin/docker-machine
 
 echo "."
-echo "*********************************************************************************
+echo "*********************************************************************************"
 echo "   Installing jq"
-echo "*********************************************************************************
+echo "*********************************************************************************"
 sudo curl -L https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 >~/jq-linux64
 sudo mv ~/jq-linux64 /usr/local/bin/jq
 sudo chmod +x /usr/local/bin/jq
 
 echo "."
-echo "*********************************************************************************
+echo "*********************************************************************************"
 echo "   Installing Ansible"
-echo "*********************************************************************************
+echo "*********************************************************************************"
 sudo apt-get install software-properties-common -y 
 sudo apt-add-repository ppa:ansible/ansible -y 
 sudo apt-get update -y 
@@ -111,16 +82,16 @@ sudo wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/invent
 sudo mv ec2.ini /etc/ansible
 
 echo "."
-echo "*********************************************************************************
+echo "*********************************************************************************"
 echo "   Installing Terraform"
-echo "*********************************************************************************
+echo "*********************************************************************************"
 sudo curl -L https://releases.hashicorp.com/terraform/0.7.13/terraform_0.7.13_linux_amd64.zip >~/terraform_0.7.13_linux_amd64.zip
 sudo unzip ~/terraform_0.7.13_linux_amd64.zip -d /usr/local/bin
 
 echo "."
-echo "*********************************************************************************
+echo "*********************************************************************************"
 echo "   Installing Packer"
-echo "*********************************************************************************
+echo "*********************************************************************************"
 sudo curl -L https://releases.hashicorp.com/packer/0.12.0/packer_0.12.0_linux_amd64.zip >~/packer_0.12.0_linux_amd64.zip
 sudo unzip ~/packer_0.12.0_linux_amd64.zip -d /usr/local/bin
 
@@ -137,30 +108,30 @@ sudo ln -s /home/vagrant/workspace/.aws /home/vagrant/.aws
 complete -C '/usr/local/aws/bin/aws_completer' aws
 
 echo "."
-echo "*********************************************************************************
-echo "   installing node.js
-echo "*********************************************************************************
+echo "*********************************************************************************"
+echo "   installing node.js"
+echo "*********************************************************************************"
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y 
 
 echo "."
-echo "*********************************************************************************
-echo "   installing gradle
-echo "*********************************************************************************
+echo "*********************************************************************************"
+echo "   installing gradle"
+echo "*********************************************************************************"
 sudo add-apt-repository ppa:cwchien/gradle -y 
 sudo apt-get update -y 
 sudo apt-get install gradle -y 
 
 echo "."
-echo "*********************************************************************************
-echo "   installing imagemagick
-echo "*********************************************************************************
+echo "*********************************************************************************"
+echo "   installing imagemagick"
+echo "*********************************************************************************"
 sudo apt-get install imagemagick -y 
 
 echo "."
-echo "*********************************************************************************
-echo "   installing PHP
-echo "*********************************************************************************
+echo "********************** ***********************************************************"
+echo "   installing PHP"
+echo "*********************************************************************************"
 sudo apt-get install build-essential libxml2-dev -y 
 #wget http://in1.php.net/distributions/php-5.3.29.tar.bz2
 #wget http://in1.php.net/distributions/php-7.0.12.tar.bz2
